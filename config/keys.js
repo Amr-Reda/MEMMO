ede72f81c6672747782cc375a54910892bbad09d
el7awy <amrreda547@gmail.com>
2018-11-12 16:46:04 +0200

    rm keys

--- config/keys.js
+++
@@ -1,8 +0,0 @@
-module.exports={
-  mongodbURI:"mongodb://root:root1234@ds155663.mlab.com:55663/memoo",
-  googleClientID:"288985229399-34293nl0l0hd67b8o8frb1hq1asedhtq.apps.googleusercontent.com",
-  googleClientSecret:"SDFXzcwpYnT0__m5fUhoimHq",
-  sessionId:"this is any text to encrypt the session id",
-  facebookClientID:"282698522384266",
-  facebookClientSecret:"dc5c40b4d576a4df6698a1ed75d5b5cd"
-}
